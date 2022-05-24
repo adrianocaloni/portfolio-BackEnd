ab42e6c5b9ae4c6c9de5ec4d0695f892597f5d77
Adriano Caloni <adrianocalonij@gmail.com>
2022-05-24 11:22:07 -0300

    Config person

--- src/main/java/ar/com/portfolio/api/models/Person.java
+++ src/main/java/ar/com/portfolio/api/models/Person.java
@@ -18,13 +18,52 @@ public class Person {
 
     private String nombre;
     private String apellido;
-    
-   @Column(name = "descripcion", length = 2048)
+
+    private String email;
+    private String celular;
+    private String localidad;
+
+    @Column(name = "descripcion", length = 2048)
     private String descripcion;
     private String titulo;
 
     @Column(name = "url_imagen", length = 2048)
     private String urlImage;
+
+    @Column(name = "url_imagen_portada", length = 2048)
+    private String urlImagePortada;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getLocalidad() {
+        return localidad;
+    }
+
+    public void setLocalidad(String localidad) {
+        this.localidad = localidad;
+    }
+
+    public String getUrlImagePortada() {
+        return urlImagePortada;
+    }
+
+    public void setUrlImagePortada(String urlImagePortada) {
+        this.urlImagePortada = urlImagePortada;
+    }
 
     public Long getId() {
         return id;
@@ -49,7 +88,7 @@ public class Person {
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
-    
+
     public String getDescripcion() {
         return descripcion;
     }
@@ -57,7 +96,7 @@ public class Person {
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
-    
+
     public String getTitulo() {
         return titulo;
     }
@@ -65,8 +104,7 @@ public class Person {
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
-    
-   
+
     public String getUrlImage() {
         return urlImage;
     }
